2d00930d10858e220381e6689bb575d4280fefe3
DandyEPurwanto <dandyekowork@gmail.com>
2024-05-02 17:11:54 +0700

    add a note

--- api/conftest.py
+++ api/conftest.py
@@ -2,5 +2,5 @@ from lib import get_secrets_login
 import pytest
 
 @pytest.fixture(scope="session")
-def secrets_login():
+def secrets_login(): #from here we will go to library
     return get_secrets_login()
